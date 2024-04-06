789be0da3654e8f0fb6ce61982be6c0f6a5b361c
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-04-06 12:00:02 +0530

    Updated app.jsc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react'
 import './App.css'
 import QRCode from 'react-qr-code'
@@ -8,11 +7,12 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState();
   const [size, setSize] = useState("150");
+
+  // generating QR code
   async function generateQR(){
     setLoading(true);
     try {
       setImg(data,size);
-      
     } catch (error) {
       console.error("Error generating QR code:", error);
     }
@@ -20,19 +20,19 @@ function App() {
       setLoading(false);
     }
   }
+
   return (
     <div className='app-container'>
       <h1>QR CODE GENERATOR</h1>
       <div>
         {loading && <p>Please wait...</p>}
-        {img && <QRCode className='qr-code-img' value={img} size={size}/>}
+        {img && <QRCode  className='qr-code-img' value={img} size={size} />}
       
       </div>
       <input type="text" id="dataInput" placeholder='Enter URL' onChange={(e) => setData(e.target.value)}/>
       <input type="text" id="sizeInput" placeholder='Enter image Size' onChange={(e)=> setSize(Number(e.target.value))}/>
       <div>
         <button className='generate-btn' onClick={generateQR}>Generate QR Code</button>
-        <button className='download-btn'>Download QR Code</button>
       </div>
     </div>
   )
